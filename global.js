aff36cbb260ef47900ce8033784dde541830338a
Leighton Estrada <lestradar@uni.edu.pe>
2025-03-13 16:14:20 -0300

    Lab03 upd1

--- global.js
+++ global.js
@@ -1,36 +1,37 @@
-/**  console.log("IT’S ALIVE!");
+/* console.log("IT’S ALIVE!");
 console.log("333")
 
 function $$ (selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
-}
+} */
 
-let navLinks = $$("nav a");
+/* let navLinks = $$("nav a");
 
-console.log(navLinks);
+// console.log(navLinks);
 
 let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
 
+// currentLink.classList.add("current");
+
 if (currentLink) { // or if (currentLink !== undefined)
-    currentLink?.classList.add("current");
-}
+    currentLink.classList.add("current");
+} */
 
-/** currentLink.classList.add("current"); **/
-
-/** console.log(currentLink); **/
-
+// console.log(currentLink);
 
 let pages = [
     {url: "", title: "Home"},
     {url: "contact/", title: "Contact"},
     {url: "projects/", title: "Projects"},
-    {url: "resume/", title: "Resume"}    
+    {url: "resume/", title: "Resume"},    
     //{url: "https://github.com/Mike-Kowalsky", title: "Projects"}
     // add the rest of your pages here
 ];
 
 let nav = document.createElement("nav");
 document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 for (let p of pages) {
     let url = p.url;
@@ -42,5 +43,5 @@ for (let p of pages) {
     nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
 }
 
-const ARE_WE_HOME = document.documentElement.classList.contains("home");
+
 
